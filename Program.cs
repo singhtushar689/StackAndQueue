9c6f614329db25f4c794e614b60e74a2d7d27a24
singhtushar689 <singhtushar689@gmail.com>
2022-12-02 11:20:31 +0530

    Stack Creation

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Wellcome to Stacks and Queues");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine("\n--------stack elements--------");
+            stack.Display();
         }
     }
 }
---
+++ Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Stack
+    {
+        Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+            Console.WriteLine("{0} is pushed to stack ", data);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "\n");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
